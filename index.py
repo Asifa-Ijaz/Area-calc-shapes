d2cf1a658bb5ad17d16ddd0dfeee456ad71fe607
Asifa-Ijaz <ejazh040@gmail.com>
2024-06-22 21:33:53 +0500

    Add files via upload

---
+++ index.py
@@ -0,0 +1,16 @@
+#area of triangle
+height = 6
+base = 3
+area_of_triangle = 0.5 * base * height
+print(area_of_triangle)
+
+#area of square
+side = 5
+area_of_square = side * side
+print(area_of_square)
+
+#area of circle
+radius = 7
+pi = 3.14
+area_of_circle = pi * radius * radius
+print(area_of_circle)
